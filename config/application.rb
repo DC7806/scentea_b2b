0641bf61765e74aaba3f284f0f225725d44ea0b3
DC <dachang.h@gmail.com>
2020-04-02 12:37:21 +0800

    Add models, views locale files

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module Rails6Base
     # Don't generate system test files.
     config.generators.system_tests = nil
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml')]
     config.i18n.default_locale = :'zh-TW'
   end
 end
