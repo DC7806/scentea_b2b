3165b73b5de93fb1e0aa3726c5035299e3742dab
DC <dachang.h@gmail.com>
2020-07-15 23:25:41 +0800

    Adjust backend webpack structure

---
+++ app/javascript/src/backend/javascripts/index.js
@@ -0,0 +1,11 @@
+// Libs
+import './lib/bootstrap.bundle.min.js'
+import './lib/jquery.dcjqaccordion.2.7.js'
+import './lib/jquery.scrollTo.min.js'
+import './lib/jquery.nicescroll'
+
+// Template
+import './template'
+
+// Modules
+import './modules/notification'
