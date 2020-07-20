caba78bc712e7b642f872e42ad93be66fa18fc8c
DC <dachang.h@gmail.com>
2020-07-20 22:36:55 +0800

    Add user sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
 
   devise_for :users,
              controllers: {
-               registrations: 'users/registrations'
+               registrations: 'users/registrations',
+               sessions: 'users/sessions'
              }
 
   root to: 'pages#index'
