c387c69383fa7d96555facab46b6144324dff949
DC <dachang.h@gmail.com>
2020-07-22 23:50:45 +0800

    Add devise pages:
    - Add registrations#edit, passwords#new, passwords#edit pages
    
    - Add user account management menu to header

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   devise_for :users,
              controllers: {
                registrations: 'users/registrations',
-               sessions: 'users/sessions'
+               sessions: 'users/sessions',
+               passwords: 'users/passwords'
              }
 
   root to: 'pages#index'
