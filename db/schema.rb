2ad15219d32b6bc260e01827595d60d52ea0e620
DC <dachang.h@gmail.com>
2020-08-02 19:58:17 +0800

    Add unique name index to categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_054505) do
+ActiveRecord::Schema.define(version: 2020_08_02_115456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_08_02_054505) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["position"], name: "index_categories_on_position"
     t.index ["region"], name: "index_categories_on_region"
+    t.index ["type", "region", "name"], name: "index_categories_on_type_and_region_and_name", unique: true, where: "((name)::text <> ''::text)"
   end
 
   create_table "faqs", force: :cascade do |t|
