95dded5f95d3db75241144fd89847b1bf793f679
DC <dachang.h@gmail.com>
2020-08-09 18:47:29 +0800

    Change inheritance for FaqCategoriesController

--- app/controllers/admin/faq_categories_controller.rb
+++ app/controllers/admin/faq_categories_controller.rb
@@ -1,45 +1,8 @@
 # frozen_string_literal: true
 
 module Admin
-  class FaqCategoriesController < AdminController
-    include Admin::Sortable
-
-    before_action :find_faq_category, only: %i[edit update destroy]
-
-    def index
-      @categories = current_scope.includes(:string_translations)
-                                 .where(region: current_region)
-                                 .order(:position)
-    end
-
-    def new
-      @category = current_scope.new(region: current_region)
-    end
-
-    def create
-      @category = current_scope.new(faq_category_params)
-      return redirect_to_index if @category.save
-
-      flash_and_render(:new, record: @category)
-    end
-
-    def edit; end
-
-    def update
-      return redirect_to_index if @category.update(faq_category_params)
-
-      flash_and_render(:edit, record: @category)
-    end
-
-    def destroy
-      redirect_to_index if @category.destroy
-    end
-
+  class FaqCategoriesController < CategoriesController
     private
-
-      def find_faq_category
-        @category = current_scope.find(params[:id])
-      end
 
       def current_scope
         @current_scope ||= FaqCategory
@@ -49,13 +12,8 @@ module Admin
         sort_admin_region_faq_categories_path
       end
 
-      def faq_category_params
-        params.require(:faq_category)
-              .permit(
-                :name_en,
-                :name_zh_tw,
-                :region
-              )
+      def current_category_param
+        :faq_category
       end
   end
 end
