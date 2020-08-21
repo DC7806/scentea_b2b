02640b8383808a24701f8093e90148f5c08282d0
DC <dachang.h@gmail.com>
2020-08-21 16:49:29 +0800

    Enhance locale

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -4,14 +4,9 @@ class FrontendController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_account!
 
-  helper_method :article_categories
-
   private
 
-    def current_account_region
-      current_user.account.region
-    end
-
+    # TODO: Make it resources specific
     def authenticate_account!
       case current_user.account.status
       when 'created'
@@ -19,17 +14,5 @@ class FrontendController < ApplicationController
       when 'pending'
         redirect_to complete_account_registration_path
       end
-    end
-
-    def default_url_options
-      { locale: I18n.locale }
-    end
-
-    def article_categories
-      @article_categories ||=
-        ArticleCategory.with_articles
-                       .includes(:string_translations)
-                       .where(region: current_account_region)
-                       .order(:position)
     end
 end
