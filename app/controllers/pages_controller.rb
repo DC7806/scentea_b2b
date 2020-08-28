bbe8a29de76ad68827a36648d175bfacbef5d50d
DC <dachang.h@gmail.com>
2020-08-29 01:41:29 +0800

    Add carousels to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class PagesController < FrontendController
-  def homepage; end
+  def homepage
+    @carousels =
+      Carousel.where(region: current_user.account.region)
+              .includes(:string_translations)
+              .order(:position)
+  end
 
   def faq
     @faq_categories =
