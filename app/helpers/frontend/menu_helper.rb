0a2c9c948a46412a6854321d7de53bbc9f8d5b50
DC <dachang.h@gmail.com>
2020-09-14 14:32:12 +0800

    Add #customization_form_url_menu to header

---
+++ app/helpers/frontend/menu_helper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Frontend
+  module MenuHelper
+    def customization_form_url_menu(list_styles: nil)
+      return if site_settings.blank?
+
+      locale = current_user.account.domestic? ? :'zh-TW' : :en
+      form_url = site_settings.customization_form_url(locale: locale)
+      return unless form_url
+
+      content_tag :li, class: list_styles do
+        link_to t('.links.customization'),
+                form_url,
+                target: :_blank,
+                rel: 'noopener nofollow'
+      end
+    end
+  end
+end
