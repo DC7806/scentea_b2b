d334276871afdcbf48b5f55f5bc9af26f4c009c3
DC <dachang.h@gmail.com>
2020-10-19 01:08:50 +0800

    Add admin page_sections CRUD

--- app/controllers/admin/page_sections_controller.rb
+++ app/controllers/admin/page_sections_controller.rb
@@ -5,15 +5,19 @@ module Admin
     include Admin::Sortable
 
     before_action :find_section, only: %i[edit update destroy]
+    before_action :section_style_options, only: %i[new create]
     helper_method :current_page
 
     def index
-      @sections = current_scope.where(region: current_region)
-                               .order(:position)
+      @sections = current_scope.where(
+        region: current_region,
+        page: current_page
+      ).order(:position)
     end
 
     def new
-      @section = current_scope.new(region: current_region)
+      @section = current_scope.new
+      @section_styles = current_scope.styles.keys
     end
 
     def create
@@ -46,17 +50,40 @@ module Admin
       end
 
       def current_scope
-        @current_scope ||= PageSection
+        return PageSection if action_name == 'index'
+
+        PageSection.includes(
+          contents: %i[string_translations text_translations]
+        )
       end
 
       def current_sort_path
         sort_admin_page_sections_path
       end
 
+      def section_style_options
+        @section_style_options ||= current_scope.styles.keys.map do |key|
+          [SectionContentStyle.find_by(style: key).name, key]
+        end
+      end
+
       def section_params
-        params.require(:section)
+        params.require(:page_section)
               .permit(
-                :region
+                :style,
+                contents_attributes: %i[
+                  description_en description_zh_tw
+                  id image
+                  link_text_en link_text_zh_tw
+                  link_url_en link_url_zh_tw
+                  subtitle_en subtitle_zh_tw
+                  title_en title_zh_tw
+                ]
+              ).merge(
+                {
+                  region: current_region,
+                  page: current_page
+                }
               )
       end
   end
